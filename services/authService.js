e44cbb5595df000eaaf9d570b84d94a4539dd44d
jehnsen <jehnsen.0321@gmail.com>
2020-11-30 21:01:21 +0800

    disabled brcypt

--- services/authService.js
+++ services/authService.js
@@ -1,27 +1,27 @@
 const User = require('../models/user');
-const bcrypt = require('bcrypt')
+// const bcrypt = require('bcrypt')
 
 exports.findUserByEmail = async ({ email }) => {
     return await User.findOne({ email: email })
 }
 
-exports.create = async ({ email, password, role }) => {
-    await User.findOne({ email: email })
-        .then(existingUser => {
-            if(!existingUser){
-                bcrypt.hash(password, 12).then(async hashedPassword => {
-                    const user = new User({
-                        email,
-                        password: hashedPassword,
-                        role
-                    })
-                    return await user.save()
-                })
+// exports.create = async ({ email, password, role }) => {
+//     await User.findOne({ email: email })
+//         .then(existingUser => {
+//             if(!existingUser){
+//                 bcrypt.hash(password, 12).then(async hashedPassword => {
+//                     const user = new User({
+//                         email,
+//                         password: hashedPassword,
+//                         role
+//                     })
+//                     return await user.save()
+//                 })
                 
-            }
-            return existingUser;
-        })
-}
+//             }
+//             return existingUser;
+//         })
+// }
 
 exports.logIn = async ({ email }) => {
     return await User.findOne({ email: email })
