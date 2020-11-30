b1c93dd9220b61f3923a800f560e9e1a213a21e3
jehnsen <jehnsen.0321@gmail.com>
2020-11-30 18:00:09 +0800

    added index route message

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', function(req, res) {
+
+    res.send('success!')
+
+});
+
 // const redisCache = require('../utils/redisCache')
 // const redisClient = require('../utils/redisClient')
 
